799a50fc6da8b8308fef46f7d0940c62906ab67c
Brad Cypert <brad.cypert@gmail.com>
2021-12-18 10:23:51 -0500

    Add supported flags, add some doc comments

--- test/usage_test.dart
+++ test/usage_test.dart
@@ -2,7 +2,11 @@ import 'package:bosun/bosun.dart';
 import 'package:test/test.dart';
 
 class AppCmd extends Command {
-  AppCmd() : super(command: 'app', description: 'run as an app shell');
+  AppCmd()
+      : super(
+            command: 'app',
+            description: 'run as an app shell',
+            supportedFlags: {'--app': 'The app that you want to use'});
 
   @override
   void run(List<String> args, Map<String, dynamic> flags) {
@@ -27,7 +31,7 @@ class RunCmd extends Command {
 void main() {
   group('Generic example', () {
     test('Supports basic example', () {
-      var args = 'run app --bar=baz'.split(' ');
+      var args = 'run app --help '.split(' ');
       execute(BosunCommand('donker', subcommands: [RunCmd()]), args);
 
       var args2 = 'run app nested --bar=baz'.split(' ');
